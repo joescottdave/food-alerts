34ace3b107c6f2a2c2c0926cfe04c37c35d516ff
joescottdave <joseph.davies@outlook.com>
2018-12-03 14:30:25 +0000

    first commit: Created the App broken into three sections based on the initial requirements.  The app makes a first call to the API for the ten most recent alerts and then generates Alert components where relevant.

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <section>
+      <h2>Single Alert</h2>
+      <Alert v-if="alerts" :desc="alerts[0].title" :date="alerts[0].created" />
+    </section>
+    <section>
+      <h2>Ten Most Recent Alerts</h2>
+      <Alert v-for="alert in alerts" :desc="alert.title" :date="alert.created"/>
+    </section>
+    <section>
+      <h2>Alert by Keyword</h2>
+      <form v-on:submit="submitThis">
+      <input v-model="input"/>
+      <button type="submit">Search</button>
+      </form>
+      <Alert v-if="searchResults" v-for="alert in searchResults" :desc="alert.title" :date="alert.created" />
+    </section>
+  </div>
+</template>
+
+<script>
+import Alert from './components/Alert.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Alert
+  },
+  data: function() {
+    return {
+      alerts: null,
+      input: null,
+      searchResults: null
+    }
+  },
+  mounted: function() {
+    fetch('http://data.food.gov.uk/food-alerts/id?_limit=10&_sort=-created')
+      .then(response => response.json())
+      .then(data => this.alerts = data.items)
+  },
+  methods: {
+    submitThis(e) {
+      e.preventDefault()
+      console.log(this.input)
+      fetch('https://data.food.gov.uk/food-alerts/id?search=' + this.input)
+        .then(response => response.json())
+        .then(data => this.searchResults = data.items)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #252525;
+  margin-top: 60px;
+}
+
+section {
+  border-bottom: 2px grey solid;
+  padding: 1em;
+}
+</style>
---
+++ src/components/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alert">
+      <h3>This is an alert.</h3>
+      <h4>{{ date }}</h4>
+      <p>{{ desc }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'Alert',
+      props: ['date', 'desc']
+  }
+</script>
+
+<style scoped>
+  h3 {
+      color: red;
+  }
+</style>
