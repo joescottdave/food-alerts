8eb59ded7e0c96f88bd857d4b3e91eb87ad3e854
joescottdave <joseph.davies@outlook.com>
2018-12-03 21:11:59 +0000

    new component for Full Alert view - once a user has clicked through from an alert preview

--- src/components/FullAlert.vue
+++ src/components/FullAlert.vue
@@ -1,9 +1,30 @@
 <template>
-  <h2>This is an alert</h2>
+  <div>
+    <ul>
+      <li>{{alert.created}}</li>
+      <li>{{alert.reportingBusiness.commonName}}</li>
+      <li v-for="product in alert.productDetails">{{product.productName}}</li>
+      <li class="allergens-item" v-for="allergen in alert.problem[0].allergen">{{allergen.label}}</li>
+      <li>{{alert.actionTaken}}</li>
+      <li>{{alert.consumerAdvice}}</li>
+    </ul>
+    <router-link to="/">Go Back</router-link>
+  </div>
 </template>
 
 <script>
   export default {
-      name: 'FullAlert'
+      name: 'FullAlert',
+      data: function() {
+          return {
+          id: this.$route.params.id,
+          alert: null
+          }
+      },
+      mounted: function() {
+          fetch("http://data.food.gov.uk/food-alerts/id/" + this.id)
+            .then(response => response.json())
+            .then(data => this.alert = data.items[0])
+      }
   }
 </script>
