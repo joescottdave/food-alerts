653b6c1ce5d1b82cf53d98e62dab729b82fa4c2d
joescottdave <joseph.davies@outlook.com>
2018-12-12 10:52:07 +0000

    adding browser history mode and build for gh-pages

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,9 @@
 </template>
 
 <script>
-import Home from './components/Home.vue'
 
 export default {
-  name: 'app',
-  components: {
-    Home
-  }
+  name: 'app'
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import FullAlert from './components/FullAlert.vue'
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
             path:'/',
